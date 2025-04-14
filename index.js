1a2a9a425b20cbe0a79f44d14bb95ac29850e019
Prakash Prasad Rai <119156734+prakash-prasad22@users.noreply.github.com>
2025-04-14 16:25:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(db.json);
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000;
+const cors = require('cors');
 
+server.use(cors()); // Enable CORS
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 8000; 
+
+server.listen(port, () => {
+  console.log(\`JSON Server is running on port \${port}\`);
+});
